cfb200445cd2395969828ea1fdf7deeabc69efbe
andresmartinez6 <andresiz11x@gmail.com>
2024-04-09 12:20:27 +0200

    Modelo Vista Controlador

---
+++ LEER/VideoClub/bd/bd.php
@@ -0,0 +1,11 @@
+<?php
+
+    class conectar{
+        public static function conexion(){
+            $conexion=new mysqli("localhost","root","","series");
+            $conexion->set_charset("utf8");
+            return $conexion;
+        }
+    }
+
+?>
